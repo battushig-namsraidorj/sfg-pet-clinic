7eacce90f57a28da52f78fedfa2a066850bbb7cc
Battushig Namsraidorj <battushig-namsraidorj@gmail.com>
2021-03-12 08:51:08 -0600

    Added Spring Data JPA Service for Vets. Closes #41

--- src/main/java/guru/springframework/sfgpetclinic/service/VetService.java
+++ src/main/java/guru/springframework/sfgpetclinic/service/VetService.java
@@ -3,5 +3,6 @@ package guru.springframework.sfgpetclinic.service;
 import guru.springframework.sfgpetclinic.model.Vet;
 
 public interface VetService extends CrudService<Vet, Long> {
+    Vet findByLastName(String lastName);
 
 }
